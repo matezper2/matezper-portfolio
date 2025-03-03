e28516fbc4f34d67e3c01f51574303605feb8468
matezper2 <133988655+matezper2@users.noreply.github.com>
2025-03-03 15:59:01 +0100

    Update nyelv.js

--- nyelv.js
+++ nyelv.js
@@ -2,10 +2,10 @@ const languageSelect = document.getElementById('language-select');
 
 languageSelect.addEventListener('change', (event) => {
     const selectedLanguage = event.target.value;
-    // Itt frissítheted a szövegeket a kiválasztott nyelv szerint
+
     if (selectedLanguage === 'hu') {
-        // Magyar szövegek
+        
     } else {
-        // Angol szövegek
+        
     }
 });
